9bd1eb43e3b1448149b978b1f20267c59e1d956c
sagar vora <hrsavaliya9@gmail.com>
2020-12-19 09:52:32 +0530

    create server and apis

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const usersRouter = require("./users/router");
+const authRouter = require("./auth/router");
 
 module.exports = (app) => {
     app.use("/users", usersRouter);
+    app.use("/auth", authRouter);
 };
--- app/users/router.js
+++ app/users/router.js
@@ -2,8 +2,6 @@ require('./model');
 const express = require("express");
 const router = express.Router();
 const controller = require('./controller');
-// Create a new Note
-router.post('/', controller.create);
 
 // Retrieve all controller
 router.get('/', controller.findAll);
@@ -19,8 +17,5 @@ router.put('/activeUsers/:id', controller.activeUsers)
 
 // Delete a Note with noteId
 router.delete('/:id', controller.delete);
-
-//login route
-router.post('/login', controller.login);
 
 module.exports = router;
